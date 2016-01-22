af6b4e16c0316aa6be988aa3ed6ad454692f1fd7
Artyom Korzun <mon5ter@list.ru>
2016-01-22 14:58:14 +0300

    polishing

--- src/main/java/org/f1x/engine/CloseCommand.java
+++ src/main/java/org/f1x/engine/CloseCommand.java
@@ -1,14 +1,6 @@
 package org.f1x.engine;
 
-import org.f1x.FIXVersion;
-import org.f1x.SessionIDBean;
-import org.f1x.log.FileMessageLog;
-import org.f1x.log.layout.TimeLayout;
 import org.f1x.util.Command;
-
-import java.net.InetSocketAddress;
-import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class CloseCommand implements Command<SessionProcessor> {
 
@@ -17,31 +9,6 @@ public class CloseCommand implements Command<SessionProcessor> {
     @Override
     public void execute(SessionProcessor processor) {
         processor.processCloseCommand(this);
-    }
-
-    public static void main(String[] args) {
-        SessionContext context = new SessionContext(
-                false,
-                new InetSocketAddress(25000),
-                new SessionIDBean("TTDEV14O", "DELTIX"),
-                FIXVersion.FIX42
-        ).log(new FileMessageLog(1 << 22, Paths.get("D:/f1x-log.messages"), new TimeLayout()));
-
-        SingleSessionEngine engine = new SingleSessionEngine(context, new SessionProcessor(context) {
-
-            @Override
-            protected void onError(Exception e) {
-                System.err.println(e.getMessage());
-                e.printStackTrace();
-            }
-
-        });
-        engine.start();
-
-        System.out.println("Close?");
-        Scanner scanner = new Scanner(System.in);
-        scanner.next();
-        engine.close();
     }
 
 }
