585ff53c82453078a88af95159c5ff0e7175bc02
Artyom Korzun <akorzun@deltixlab.com>
2017-03-31 18:53:08 +0300

    + 1 check sum benchmark

--- src/jmh/java/org/efix/sandbox/CheckSumBenchmark.java
+++ src/jmh/java/org/efix/sandbox/CheckSumBenchmark.java
@@ -71,6 +71,46 @@ public class CheckSumBenchmark {
         return checkSum & 0xFF;
     }
 
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public int memoryWithAddressUnrolledBy16() {
+        UnsafeBuffer buffer = Config.buffer;
+        long address = buffer.addressOffset() + Config.offset;
+        int length = Config.length;
+        long end = address + length;
+        long limit = end & 0xFFFF_FFF0;
+
+        int checkSum = 0;
+
+        while (address < limit) {
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 1);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 2);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 3);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 4);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 5);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 6);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 7);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 8);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 9);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 10);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 11);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 12);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 13);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 14);
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address + 15);
+            address += 16;
+        }
+
+        while (address < end) {
+            checkSum ^= UnsafeAccess.UNSAFE.getByte(address);
+            address++;
+        }
+
+
+        return checkSum & 0xFF;
+    }
+
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(CheckSumBenchmark.class.getSimpleName())
